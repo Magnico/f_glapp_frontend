a5eb3ce605251e1864f86e3edb07e4741e6c53f6
isabella02 <82828398+isabella02@users.noreply.github.com>
2022-10-16 12:27:22 -0500

    map

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,51 +1,26 @@
 import 'package:f_shopping_app/ui/pages/product_list.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../Widgets/banner.dart';
 import '../Widgets/cart_total.dart';
 
-class HomePage extends StatelessWidget {
+const LatLng currentLocation = LatLng(25.1193, 55.3773);
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
+  
   @override
+  State<HomePage> createState() => HomeState();
+  
+}
+
+class HomeState extends State<HomePage>{
+@override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Icon(Icons.abc,size:80, color: Colors.amber));
-  }
-
-  Widget buildProfileImage() {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: const [
-        CustomBanner(200),
-        CircleAvatar(
-          backgroundImage:
-              NetworkImage("https://randomuser.me/api/portraits/men/75.jpg"),
-          radius: 60.0,
-        )
-      ],
-    );
-  }
-
-  Widget customAppBar() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GestureDetector(
-            onTap: () => Get.to(() => ProductList(),
-                transition: Transition.circularReveal,
-                duration: const Duration(seconds: 1)),
-            child: const Icon(
-              Icons.shopping_cart,
-              size: 30,
-              color: Colors.white,
-            ),
-          ),
-        )
-      ],
-    );
+    return const Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: currentLocation,)),
+        );
   }
 }
